9b96f4bc8824cb9fd97f8946d252c047d4f6e415
Sebastian Deseke <deseke97@gmail.com>
2024-06-27 13:59:15 +0200

    Initial Commit

---
+++ FileSort.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NumberBlockSorting
+{
+    public class FileSort
+    {
+        public List<string> ReadPhoneNumbersFromFile(string filename)
+        {
+            List<string> phoneNumbers = new List<string>();
+            try
+            {
+                using (StreamReader file = new StreamReader(filename))
+                {
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        phoneNumbers.Add(int.Parse(line));
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Error: The file {filename} was not found.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: The file contains non-numeric values.");
+            }
+            return phoneNumbers;
+        }
+
+        public List<string> FindConsecutiveNumbers(List<string> numbers)
+        {
+            numbers.Sort();
+            List<string> consecutiveSequences = new List<string>();
+            List<string> currentSequence = new List<string>();
+
+            foreach (var number in numbers)
+            {
+                if (currentSequence.Count == 0 || number == currentSequence[currentSequence.Count - 1] + 1)
+                {
+                    currentSequence.Add(number);
+                }
+                else
+                {
+                    if (currentSequence.Count > 1)
+                    {
+                        consecutiveSequences.Add($"{currentSequence[0]}, {currentSequence[currentSequence.Count - 1]}");
+                    }
+                    else
+                    {
+                        consecutiveSequences.Add($"{currentSequence[0]}, {currentSequence[0]}");
+                    }
+                }
+                currentSequence = new List<string> { number };
+            }
+        }
+            // Check for the last sequence
+            if (currentSequence.Length > 1)
+            {
+                consecutiveSequences.Add($"{currentSequence[0]}, {currentSequence[-1]}");
+            }
+            else
+            {
+                consecutiveSequences.Add($"{currentSequence[0]}, {currentSequence[0]}");
+            }
+        return consecutiveSequences;
+        }
+
+    }
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NumberBlockSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilename = "phoneNumbers.txt";
+            string outputFilename = "output.txt";
+
+            FileSort fileSort = new FileSort();
+
+            List<string> phoneNumbers = fileSort.ReadPhoneNumbersFromFile(inputFilename);
+
+            if (phoneNumbers.Count > 0)
+            {
+                List<string> consecutiveNumbers = fileSort.FindConsecutiveNumbers(phoneNumbers);
+
+                using (StreamWriter file = new StreamWriter(outputFilename))
+                {
+                    file.WriteLine("Consecutive number blocks:\n");
+                    foreach (var number in consecutiveNumber)
+                    {
+                        file.WriteLine(number);
+                    }
+                }
+                int totalNumbers = phoneNumbers.Count;
+                Console.WriteLine($"Saved the numbers to {outputFilename}.");
+                Console.WriteLine($"Found {totalNumbers} phone numbers in the file.");
+            }
+            else
+            {
+                Console.WriteLine("No phone numbers were found in the file.");
+            }
+        }
+    }
+}
